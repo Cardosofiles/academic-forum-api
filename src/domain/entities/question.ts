6c82fef322d24435560b0e7b360914e1ceea2b90
João Batista <cardosofiles@outlook.com>
2026-08-21 16:21:29 -0300

    feat(use-cases-entities): add a basic methods of all classes

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,6 +1,6 @@
-import { randomUUID } from "node:crypto";
-
 import type { Slug } from "@/domain/entities/value-objects/slug.js";
+
+import { Entity } from "@/core/entities/entity.js";
 
 interface QuestionProps {
   title: string;
@@ -9,18 +9,8 @@ interface QuestionProps {
   authorId: string;
 }
 
-export class Question {
-  public id: string;
-  public title: string;
-  public content: string;
-  public slug: Slug;
-  public authorId: string;
-
+export class Question extends Entity<QuestionProps> {
   constructor(props: QuestionProps, id?: string) {
-    this.title = props.title;
-    this.content = props.content;
-    this.slug = props.slug;
-    this.authorId = props.authorId;
-    this.id = id ?? randomUUID();
+    super(props, id);
   }
 }
