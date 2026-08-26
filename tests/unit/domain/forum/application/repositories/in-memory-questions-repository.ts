ad44569bbc12943c8bd8dc9287f7623ea89df62d
João Batista <cardosofiles@outlook.com>
2026-08-26 15:40:19 -0300

    feat(unit-test): adds unit test for deleting questions

--- tests/unit/domain/forum/application/repositories/in-memory-questions-repository.ts
+++ tests/unit/domain/forum/application/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,16 @@ import type { Question } from '@/domain/forum/enterprise/entities/question';
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id);
+
+    if (!question) {
+      return Promise.resolve(null);
+    }
+
+    return question;
+  }
+
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.items.find((item) => item.slug.value === slug);
 
@@ -16,6 +26,17 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question): Promise<void> {
     this.items.push(question);
+    return Promise.resolve();
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    if (itemIndex === -1) {
+      throw new Error('Question not found.');
+    }
+
+    this.items.splice(itemIndex, 1);
     return Promise.resolve();
   }
 }
---
+++ tests/unit/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,56 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question';
+import { makeQuestion } from '@test/unit/domain/forum/application/factories/make-question';
+import { InMemoryQuestionsRepository } from '@test/unit/domain/forum/application/repositories/in-memory-questions-repository';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: DeleteQuestionUseCase;
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository);
+  });
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    );
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    await sut.execute({
+      questionId: 'question-1',
+      authorId: 'author-1',
+    });
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+  });
+
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    );
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    expect(() => {
+      return sut.execute({
+        questionId: 'question-1',
+        authorId: 'author-2',
+      });
+    })
+      .rejects.toBeInstanceOf(Error)
+      .catch((error) => {
+        expect(error).toBe('Not allowed.');
+      });
+  });
+});
